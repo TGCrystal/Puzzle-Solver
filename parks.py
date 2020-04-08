9185f69e586f0fd3399306c374d8bb8811cf7800
Trevor <TGCrystal@users.noreply.github.com>
2020-04-08 00:30:43 -0400

    Finished basic (somewhat) multithreaded A*

--- parks.py
+++ parks.py
@@ -1,17 +1,23 @@
 import time
 import threading
 from queue import PriorityQueue
+import copy
+
+
+availableActions = PriorityQueue()
 
 
 class ParkAnswer:
     def __init__(self, board):
         self.rowsAvailable = list(range(0, len(board)))
-        self.columnsAvailable = list(range(0, len(board[0])))
+        self.columnsAvailable = list(range(0, len(board[0])-1)) #-1 to account for \n character
         self.colorsAvailable = set()
         self.placedTrees = []
         self.board = board
+        # print(self.rowsAvailable, self.columnsAvailable)
         for i in self.rowsAvailable:
-            for j in self.columnsAvailablej:
+            for j in self.columnsAvailable:
+                # print(i, j)
                 self.colorsAvailable.add(board[i][j])
 
     def placeTree(self, row, column):
@@ -27,7 +33,7 @@ class ParkAnswer:
         return self.placedTrees
 
     def __copy__(self):
-        cpy = type(self)()
+        cpy = type(self)(self.board)
         cpy.rowsAvailable = self.rowsAvailable.copy()
         cpy.columnsAvailable = self.columnsAvailable.copy()
         cpy.colorsAvailable = self.colorsAvailable.copy()
@@ -35,19 +41,18 @@ class ParkAnswer:
         cpy.board = self.board
         return cpy
 
-
-availableActions = PriorityQueue()
-
+    def __lt__(self, other):
+        return True
 
 def heuristic(action):
     return 0
 
 
-def getActions(board, partialAnswer):
+def getActions(partialAnswer):
     for i in partialAnswer[1].rowsAvailable:
         for j in partialAnswer[1].columnsAvailable:
-            if board[i][j] in partialAnswer[1].colorsAvailable:
-                newAction = partialAnswer[1].copy()
+            if partialAnswer[1].board[i][j] in partialAnswer[1].colorsAvailable:
+                newAction = copy.copy(partialAnswer[1])
                 newAction.placeTree(i, j)
                 availableActions.put((heuristic(newAction) + partialAnswer[0] + 1, newAction))
 
@@ -63,12 +68,21 @@ def loadfile(fileName):
 
 
 def main():
-    start_time = time.time()
+    startTime = time.time()
     board = loadfile("park1")
     blankAnswer = ParkAnswer(board)
-    getActions(blankAnswer)
-    # while True:
-        
+    threading.Thread(target=getActions, args=((0, blankAnswer),)).start()
+    actionsExpanded = 1
+    answer = None
+    while True:
+        testAnswer = availableActions.get()
+        actionsExpanded += 1
+        if testAnswer[1].isGoal():
+            answer = testAnswer[1].getAnswer()
+            break
+        threading.Thread(target=getActions, args=(testAnswer,)).start()
+    print(actionsExpanded, time.time()-startTime)
+    print(answer)
 
 
 if __name__ == "__main__":
