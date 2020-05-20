0c60fe1f560ecc1ffd54f71314ca34a6a3787ba6
Trevor <TGCrystal@users.noreply.github.com>
2020-05-20 16:09:26 -0400

    Moved core code to new file

--- parks.py
+++ parks.py
@@ -1,13 +1,5 @@
-import time
+from base import Answer, Solver
 import copy
-
-
-class Answer:
-    def getAnswer(self):
-        pass
-
-    def __copy__(self):
-        pass
 
 
 class ParkAnswer(Answer):
@@ -44,42 +36,6 @@ class ParkAnswer(Answer):
         cpy.colorsAvailable = self.colorsAvailable.copy()
         cpy.placedTrees = self.placedTrees.copy()
         return cpy
-
-
-class Solver:
-    def loadFile(self, fileName, numInputSplits):
-        with open(fileName, 'r') as file:
-            rawInput = file.read().split('\n', numInputSplits)
-            rawInput[numInputSplits] = rawInput[numInputSplits].split('\n')
-        return rawInput
-
-    def getActions(self, partialAnswer):
-        pass
-
-    def isGoal(self, partialAnswer):
-        pass
-
-    def depthFirstSolve(self):
-        possibleActions = self.getActions(self.blankAnswer) # possibleActions will be a list
-        answer = None
-        actionsExpanded = 0
-        startTime = time.time()
-        endTime = startTime
-
-        while len(possibleActions) > 0:
-            testAnswer = possibleActions.pop()
-            actionsExpanded += 1
-            if self.isGoal(testAnswer):
-                answer = testAnswer.getAnswer()
-                endTime = time.time()
-                break
-            possibleActions += self.getActions(testAnswer)
-
-        if answer is None:
-            print("No answer found")
-        else:
-            print(answer)
-            print("Found in {} seconds with {} actions checked".format(endTime-startTime, actionsExpanded))
 
 
 class ParkSolver(Solver):
